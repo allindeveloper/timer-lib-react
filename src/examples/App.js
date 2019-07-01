c83391137aa9a373f23eea6a2ca7875b05553193
Uchendu Precious <uchendubozz@gmail.com>
2019-07-01 13:29:59 +0100

    First Commit

--- src/examples/App.js
+++ src/examples/App.js
@@ -1,56 +1,22 @@
-import React, { Component } from "react";
-import { Nigeria, Abia,Lagos, AkwaIbom, Anambra, Bauchi, Bayelsa, Kaduna } from "../lib";
-import Ekiti from "../lib/components/Ekiti";
-
+import React from "react";
+import {Timer} from "../lib";
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    
+    }
+
+  }
   render() {
-    const state = "State Info";
-    const val = 55;
-    const abiaData =
-      '<div class="hoverinfo">' +
-      "<h6>" +
-      state +
-      " </h6>" +
-      "Total No Of Farms - " +
-      val +
-      "" +
-      "</div>";
-    const lagosData =
-      '<div class="hoverinfo">' +
-      "<h6>" +
-      state +
-      " </h6>" +
-      "Total No Of Farms - " +
-      val +
-      "" +
-      "</div>";
-      let plateauData =  '<div class="hoverinfo">' +
-      "<h3>Edo State</h3>"+
-      "<h5>PVHHTarget - " + 14000 +"</h5>"+
-      "<h5>ZoneName - " + 34030+"</h5>"+
-      "" +
-      "</div>";
+    
     return (
       <div>
-        <Nigeria
-          stateName="delta"
-          hoverColor="red"
-          hoverBackgroundColor="grey"
-          renderData={plateauData}
-          defaultColor="green"
-          id="lag"
-        />
-        {/* <Abia
-        />
-        <Lagos
-          hoverColor="red"
-          hoverBackgroundColor="grey"
-          renderData={lagosData}
-          defaultColor="green"
-          id="lag"
-        /> */}
+       <Timer
+       />
+        
       </div>
-    )
+    );
   }
 }
 
